2f8d55240e05f3cbd8fdc858c682ede38791258c
Morato <victormoratovideos@gmail.com>
2024-10-17 22:05:20 -0300

    Atualizações na página de registro de ordem de serviço e estilizações

--- assistencia-tecnica-frontend/src/App.js
+++ assistencia-tecnica-frontend/src/App.js
@@ -5,15 +5,22 @@ import RegistroOs from './components/RegistroOs';
 import ListaOrdens from './components/ListaOrdens';
 import './styles.css'; // Importação do arquivo de estilos
 
-function App() {  // Verifique se você tem apenas uma declaração de 'App'
+function App() {
     return (
         <div>
-            <h1>Sistema de Gestão de Assistência Técnica</h1>
-            <RegistroOs /> 
+            <h1>
+                <span>Sistema de Gestão</span><br />
+                <span>de Assistência Técnica</span>
+            </h1>
+            <div className="phone-container">
+                <img src="https://images.unsplash.com/photo-1510717029971-27681a0e9da9?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="Aparelho" className="phone" />
+            </div>
+            <RegistroOs class="register-os" /> 
             <ListaOrdens />
-
         </div>
     );
 }
+
+
 
 export default App; // Certifique-se de que esta é a única exportação do arquivo
